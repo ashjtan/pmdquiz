dd847012fc8aa4aadfae3b53f10468652ee5fe0d
Ashley Tan <tan.ashley32@gmail.com>
2020-01-19 14:39:07 -0500

    add db reference

--- app/src/main/java/ashtan/pmdquiz/MainActivity.java
+++ app/src/main/java/ashtan/pmdquiz/MainActivity.java
@@ -3,7 +3,11 @@ package ashtan.pmdquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference db = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
