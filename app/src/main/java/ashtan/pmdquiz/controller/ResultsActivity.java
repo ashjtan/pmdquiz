668fff811f790c20a19e2f8f192c04131f996e7a
Ashley Tan <tan.ashley32@gmail.com>
2020-01-20 14:27:16 -0500

    added ResultsActivity
    **no transition yet

---
+++ app/src/main/java/ashtan/pmdquiz/controller/ResultsActivity.java
@@ -0,0 +1,15 @@
+package ashtan.pmdquiz.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ashtan.pmdquiz.R;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
