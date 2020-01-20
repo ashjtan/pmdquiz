5d16cc7d251f57dcfcb58d2af3c79b2b67910f8f
Ashley Tan <tan.ashley32@gmail.com>
2020-01-20 16:11:41 -0500

    added randomized questions + quiz logic

--- app/src/main/java/ashtan/pmdquiz/controller/QuestionActivity.java
+++ app/src/main/java/ashtan/pmdquiz/controller/QuestionActivity.java
@@ -1,71 +1,91 @@
 package ashtan.pmdquiz.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
+import java.util.List;
 
 import ashtan.pmdquiz.R;
-import ashtan.pmdquiz.model.Result;
+import ashtan.pmdquiz.model.Nature;
+import ashtan.pmdquiz.model.Question;
 
 public class QuestionActivity extends AppCompatActivity {
 
-    private DatabaseReference questionsDb = MainActivity.db.getReference("questions");
-
-    private ArrayList<Result> questions = new ArrayList<>();
-
-
+    Question currQ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
 
-        TextView qText = (TextView) findViewById(R.id.q_text);
-        Button opt1 = (Button) findViewById(R.id.opt1);
-        Button opt2 = (Button) findViewById(R.id.opt2);
+        setContentView(R.layout.activity_question);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-/*        resultsDb.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        //get question data
+        currQ = MainActivity.questions[MainActivity.selectedQs[MainActivity.currQNum]];
 
-                for(DataSnapshot resultSnapshot : dataSnapshot.getChildren()) {
-                    Result curr = resultSnapshot.getValue(Result.class);
-                    results.add(curr);
+        //set display
+        TextView qText = (TextView) findViewById(R.id.q_text);
+        String displayQText = MainActivity.currQNum + 1 + ". " + currQ.getText();   //+1 to acct for 0-indexing
+        qText.setText(displayQText);
 
-                    //System.out.println(results.get(0).displayName + " " + results.get(0).pokemon);
-                }
+        Button opt1 = (Button) findViewById(R.id.opt1);
+        opt1.setText(currQ.getChoices().first);
 
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.w("loadResults:onCancelled", databaseError.toException());
-            }
-        });*/
-
+        Button opt2 = (Button) findViewById(R.id.opt2);
+        opt2.setText(currQ.getChoices().second);
     }
 
-    //EVENT HANDLER
-    public void select1(View v) {
 
+
+
+    //Event Handlers
+    public void select1(View v) {
+        updateQuizResults(0);
+
+        MainActivity.currQNum++;
+        nextScreen();
     }
 
     public void select2(View v) {
+        updateQuizResults(1);
 
+        MainActivity.currQNum++;
+        nextScreen();
+    }
+
+
+
+
+    //Helpers
+    private void nextScreen() {
+        if (MainActivity.currQNum >= MainActivity.selectedQs.length) {
+            startActivity(new Intent(QuestionActivity.this, ResultsActivity.class));
+        } else {
+            startActivity(new Intent(QuestionActivity.this, QuestionActivity.class));
+        }
+    }
+
+    private void updateQuizResults(int leftOrRight) {   //0 = left, 1 = right
+        List<Pair<Nature,Integer>> resultsToAdd;
+        if (leftOrRight == 0) {
+            resultsToAdd = currQ.getResults().first;
+        } else {
+            resultsToAdd = currQ.getResults().second;
+        }
+
+        //add nature points to cumulative results
+        for (Pair<Nature, Integer> p : resultsToAdd) {
+            MainActivity.currQuizResults.put(
+                    p.first, MainActivity.currQuizResults.get(p.first) + p.second);
+        }
     }
 }
--- app/src/main/java/ashtan/pmdquiz/controller/ResultsActivity.java
+++ app/src/main/java/ashtan/pmdquiz/controller/ResultsActivity.java
@@ -2,14 +2,51 @@ package ashtan.pmdquiz.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import ashtan.pmdquiz.R;
+import ashtan.pmdquiz.model.Result;
 
 public class ResultsActivity extends AppCompatActivity {
+    private DatabaseReference resultsDb = FirebaseDatabase.getInstance().getReference("results");
+
+    private ArrayList<Result> results = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        resultsDb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot resultSnapshot : dataSnapshot.getChildren()) {
+                    Result curr = resultSnapshot.getValue(Result.class);
+                    results.add(curr);
+
+                    //System.out.println(results.get(0).displayName + " " + results.get(0).pokemon);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("loadResults:onCancelled", databaseError.toException());
+            }
+
+        });
     }
 }
