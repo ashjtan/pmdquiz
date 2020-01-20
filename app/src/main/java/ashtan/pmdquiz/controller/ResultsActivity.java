adc51eaf084fb925a34efbdeb649bc5504b0d47b
Ashley Tan <tan.ashley32@gmail.com>
2020-01-20 17:29:41 -0500

    added result calculation logic

--- app/src/main/java/ashtan/pmdquiz/controller/ResultsActivity.java
+++ app/src/main/java/ashtan/pmdquiz/controller/ResultsActivity.java
@@ -3,6 +3,9 @@ package ashtan.pmdquiz.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,6 +16,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 import ashtan.pmdquiz.R;
+import ashtan.pmdquiz.model.Nature;
 import ashtan.pmdquiz.model.Result;
 
 public class ResultsActivity extends AppCompatActivity {
@@ -30,6 +34,12 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+        TextView result = (TextView) findViewById(R.id.poke_result);
+        result.setText(MainActivity.pokemon.get(maxNature()));      //sets to pokemon name
+
+        TableLayout friendResults = (TableLayout) findViewById(R.id.friend_results);
+
+        
         resultsDb.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -37,7 +47,6 @@ public class ResultsActivity extends AppCompatActivity {
                 for(DataSnapshot resultSnapshot : dataSnapshot.getChildren()) {
                     Result curr = resultSnapshot.getValue(Result.class);
                     results.add(curr);
-
                     //System.out.println(results.get(0).displayName + " " + results.get(0).pokemon);
                 }
             }
@@ -46,7 +55,21 @@ public class ResultsActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
                 Log.w("loadResults:onCancelled", databaseError.toException());
             }
-
         });
+    }
+
+    //Helpers
+
+    //returns nature w/ max points
+    private Nature maxNature() {
+        Nature maxNature = Nature.BOLD;
+
+        for (Nature n : Nature.values()) {
+            if (MainActivity.currQuizResults.get(n) > MainActivity.currQuizResults.get(maxNature)) {
+                maxNature = n;
+            }
+        }
+
+        return maxNature;
     }
 }
